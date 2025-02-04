4fb068a115d8176121d6c824b6975e0e6ad1fcd6
Ren Williams <vadkakr@gmail.com>
2025-02-04 20:55:03 +0700

    yaya

---
+++ test1.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Ren on 04.02.2025.
+//
+
+#include "test1.h"
---
+++ test1.h
@@ -0,0 +1,16 @@
+//
+// Created by Ren on 04.02.2025.
+//
+
+#ifndef TEST1_H
+#define TEST1_H
+
+
+
+class test1 {
+
+};
+
+
+
+#endif //TEST1_H
